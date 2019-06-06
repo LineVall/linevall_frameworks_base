803a223458551be17d588c12d59fe067598497d4
Anushek Prasal <anushekprasal@gmail.com>
2019-06-06 14:18:24 +0530

    Add toggle to disable charging animation [1/2]
    @neobuddy89: Use same toggle for wireless charging animation too.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5654,6 +5654,12 @@ public final class Settings {
         public static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
 
         /**
+         * Whether to show charging animation
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION = "charging_animation";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *

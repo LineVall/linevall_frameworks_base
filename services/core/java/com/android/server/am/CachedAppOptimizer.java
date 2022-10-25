10d5916f7deb69d004218fb2e877b82037ffa555
Akash Srivastava <akashniki@gmail.com>
2022-10-26 03:17:04 +0700

    core: Integrate CLO boost fw to AOSP boost fw
    * This works just as well as it does on CLO. Ofc relies on CLO's Perf libs
    
    Change-Id: Icc2f6d5aa244ffcc43931c993d1a116ff7ec0bf5
    
    perf: Add lock protection for initialization in ScrollOptimizer
    
    As the initialization function can be accessed from multiple threads,
    need to add lock protection to avoid data race issue.
    
    Change-Id: I22e9ae8392cdc00e36131b257e24bb17e9d8e361
    CRs-Fixed: 3343100
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    
    Filter multi-layer cases for pre-rendering
    
    Multi-layer rendering can result in unexpected pending between UI
    thread and render thread if pre-rendering enabled. Need to disable
    pre-rendering for multi-layer cases.
    
    Change-Id: I569e6eb77e4feb2071b9f172d3d9ea9e7eddbacb
    CRs-Fixed: 3333436
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    
    OverScroller: Import BoostFramework.ScrollOptimizer
    
    Change-Id: I1faf475d3b935b0b9a62c5ae4225d04b078004d9
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    
    BoostFramework: Guard it with overlay
    
    Change-Id: I5e22a590b0f1f0e9a678d45d06bb262021cf84bb
    (cherry picked from commit e03f9fddff5a7e952f161063cd194372328180b5)
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    BoostFramework: Kill logcat spam on devices without QPerformance
    
    Change-Id: I6deb45a60e816fa4beceb4a37aec666935927669
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    
    mnri: scrolloptimizer and choreographer changes, refactored for aosp boost framework, fallback to AOSP boost fw if qti jars doesnt exists
    
    Co-authored-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: jhenrique09 <jhsv09@gmail.com>
    Co-authored-by: Jun Wang <quic_junwan@quicinc.com>
    Co-authored-by: xyyx <xyyx@mail.ru>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/am/CachedAppOptimizer.java
+++ services/core/java/com/android/server/am/CachedAppOptimizer.java
@@ -937,6 +937,7 @@ public final class CachedAppOptimizer {
 
                 Process.setThreadGroupAndCpuset(mCachedAppOptimizerThread.getThreadId(),
                     mCompactionPriority);
+
             } else {
                 Slog.d(TAG_AM, "Freezer disabled");
                 enableFreezer(false);

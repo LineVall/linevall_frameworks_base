3d62f48ba949d2ee3ae553ce3e9e6f573c6b716e
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-19 00:40:47 +0800

    SystemUI: Implement Island notification feature [1/2]
    * slimmed down version of reticker
    
    Co-authored-by: Dil3mm4 <dil3mm4.dev@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6657,6 +6657,11 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
 
         /**
+         * @hide
+         */
+        public static final String ISLAND_NOTIFICATION = "island_notification";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *

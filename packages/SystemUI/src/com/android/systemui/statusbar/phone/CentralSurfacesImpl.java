dd158001b79b11d5647f5c5854b0f1ac55df6825
Dhina17 <dhinalogu@gmail.com>
2024-12-27 13:11:18 +0530

    Revert "[sb] remove unused param from initializer callback"
    This reverts commit d40257cfd6d35ad5a398796cd17d6ba38bba0ec5.
    
    Needed for "SystemUI: Port statusbar brightness control".
    
    Change-Id: I6add5c20a02f5c83c7a5da907fada5265ff32abc

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -392,6 +392,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
 
     private final Point mCurrentDisplaySize = new Point();
 
+    protected PhoneStatusBarView mStatusBarView;
     private PhoneStatusBarViewController mPhoneStatusBarViewController;
     private PhoneStatusBarTransitions mStatusBarTransitions;
     private final AuthRippleController mAuthRippleController;
@@ -1313,8 +1314,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
 
             // Set up CollapsedStatusBarFragment and PhoneStatusBarView
             mStatusBarInitializer.setStatusBarViewUpdatedListener(
-                    (statusBarViewController, statusBarTransitions) -> {
-
+                    (statusBarView, statusBarViewController, statusBarTransitions) -> {
+                        mStatusBarView = statusBarView;
                         mPhoneStatusBarViewController = statusBarViewController;
                         mStatusBarTransitions = statusBarTransitions;
                         getNotificationShadeWindowViewController()
